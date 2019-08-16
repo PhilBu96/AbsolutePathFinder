24e276e0718a206c82f09bc90567240aeba81ca0
Philipp Buthmann <philipp.buthmann@uksh.de>
2019-08-16 10:51:11 +0200

    Fehler-Handling

--- AbsolutePathFinder/Program.cs
+++ AbsolutePathFinder/Program.cs
@@ -9,10 +9,19 @@ namespace AbsolutePathFinder
         static void Main(string[] args)
         {
             Console.WriteLine("Programm wird gestartet...\n");
-            Console.WriteLine("Folgende(r) Pfade(e) wurde(n) erkannt und in die Zwischenablage kopiert:\n");
-            Console.WriteLine(GetAbsoluteFilename(args) + "\n");
-            Console.WriteLine("Enter drücken zum Beenden!");
-            Console.ReadLine();
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Keine Dateien gefunden!\n");
+                Console.WriteLine("Enter drücken zum Beenden!\n");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Folgende(r) Pfade(e) wurde(n) erkannt und in die Zwischenablage kopiert:\n");
+                Console.WriteLine(GetAbsoluteFilename(args) + "\n");
+                Console.WriteLine("Enter drücken zum Beenden!");
+                Console.ReadLine();
+            }
         }
 
         static String GetAbsoluteFilename(string[] args)
@@ -21,7 +30,7 @@ namespace AbsolutePathFinder
 
             foreach (var item in args)
             {
-                temp += item;
+                temp += item + "\n";
             }
 
             Clipboard.SetText(temp);
