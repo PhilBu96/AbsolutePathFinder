d393e467b18aaa01521cca388a3464f65cb29bb7
Philipp Buthmann <philipp.buthmann@uksh.de>
2019-08-16 10:43:46 +0200

    Grundfunktionen hinzugefügt

--- AbsolutePathFinder/Program.cs
+++ AbsolutePathFinder/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AbsolutePathFinder
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Console.WriteLine("Programm wird gestartet...\n");
+            Console.WriteLine("Folgende(r) Pfade(e) wurde(n) erkannt und in die Zwischenablage kopiert:\n");
+            Console.WriteLine(GetAbsoluteFilename(args) + "\n");
+            Console.WriteLine("Enter drücken zum Beenden!");
+            Console.ReadLine();
+        }
+
+        static String GetAbsoluteFilename(string[] args)
+        {
+            string temp = "";
+
+            foreach (var item in args)
+            {
+                temp += item;
+            }
+
+            Clipboard.SetText(temp);
+            return temp;
         }
     }
 }
